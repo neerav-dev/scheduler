bebe43d6ecf2844f10d832b719c0e983846c2728
Neerav Patel <nickpatel.developer@gmail.com>
2021-06-09 00:10:06 -0400

    display appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,19 @@
 import React from "react";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
+
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show name={props.interview.student} interviewer={props.interview.interviewer} />
+      ) : (
+        <Empty />
+      )}
+    </article>
   );
 }
